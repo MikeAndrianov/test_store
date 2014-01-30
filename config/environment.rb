3fc21f0c8828ffc7b0e93b5c852c484971f3e51e
Mike Andrianov <mikeandrianov@gmail.com>
2014-01-30 23:45:24 +0300

    customized errors presentation on form

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 TestStore::Application.initialize!
+
+
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance_tag|
+  "<div class='has-error'>#{html_tag}</div>".html_safe
+end
