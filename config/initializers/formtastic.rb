343dc189a8844aaeb82d5347e65ed950c210a187
Mike Andrianov <mikeandrianov@gmail.com>
2014-02-05 16:33:38 +0300

    Monkeypatch for ActiveAdmin select form for has_many associations

---
+++ config/initializers/formtastic.rb
@@ -0,0 +1,12 @@
+module Formtastic
+  module Inputs
+    class SelectInput
+      def multiple_with_options_fix?
+        return false if options[:multiple] === false
+        multiple_without_options_fix?
+      end
+
+      alias_method_chain :multiple?, :options_fix
+    end
+  end
+end
