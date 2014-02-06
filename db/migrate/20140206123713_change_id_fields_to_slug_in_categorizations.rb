85b109274c5892d0a144b796c1bce93d31aa5c18
Mike Andrianov <mikeandrianov@gmail.com>
2014-02-07 00:51:24 +0300

    removed unnessesary migration

--- db/migrate/20140206123713_change_id_fields_to_slug_in_categorizations.rb
+++
@@ -1,6 +0,0 @@
-class ChangeIdFieldsToSlugInCategorizations < ActiveRecord::Migration
-  def change    
-    add_column :categorizations, :product_slug, :string
-    add_column :categorizations, :category_slug, :string
-  end
-end
